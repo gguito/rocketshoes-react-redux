a74ddbe2ce14fce1ed41e9c43fe3a73ae87fd6d3
gguito <gui155@hotmail.com>
2020-03-12 20:41:20 -0300

    web

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+import GlobalStyle from './styles/global';
+
+import Header from './components/Header/index';
+import Routes from './routes';
+
+import './config/reactotronConfig';
+
+import history from './services/history';
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router history={history}>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+        <ToastContainer autoClose={3000} />
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { MdShoppingBasket } from 'react-icons/md';
+
+import { Container, Cart } from './styles';
+
+import logo from '../../assets/images/logo.svg';
+
+function Header({ cartSize }) {
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="Rocketshoes" />
+      </Link>
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          {cartSize > 1 ? (
+            <span>{cartSize} itens</span>
+          ) : (
+            <span>{cartSize} item</span>
+          )}
+        </div>
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
+    </Container>
+  );
+}
+
+export default connect(state => ({
+  cartSize: state.cart.length,
+}))(Header);
